4531c6080f71f06adfb7ad5851e79638042102be
Michał Lejwoda <michal.lejwoda@gmail.com>
2026-03-22 15:54:00 +0100

    init http 1 and 2 and websockets

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang_easy_projects
+
+go 1.26.1
---
+++ http_server_1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Test")
+	fmt.Println(Hello("Michał"))
+	fmt.Println("Koniec testy")
+}
