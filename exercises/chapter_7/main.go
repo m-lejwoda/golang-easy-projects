df4b314de25c94b5a66a46f1857d594eefda11f0
Michał <michal.lejwoda@gmail.com>
2026-05-30 23:06:43 +0200

    chapter_7 2 exercises

---
+++ exercises/chapter_7/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	// Przykład użycia, żeby sprawdzić czy działa:
+	liga := League{
+		Teams: []Team{},
+		Wins:  make(map[string]int),
+	}
+
+	liga.MatchResult("Gryfy", 5, "Smoki", 3)
+	liga.MatchResult("Wilki", 2, "Gryfy", 4)
+	liga.MatchResult("Smoki", 6, "Wilki", 1)
+
+	liga.Ranking()
+}
+
+type Team struct {
+	TeamName string
+	LastName string
+}
+
+type League struct {
+	Teams []Team
+	Wins  map[string]int
+}
+
+func (l League) MatchResult(firstTeam string, FirstNumOfPoints int, secondTeam string, SecondNumOfPoints int) {
+	if FirstNumOfPoints > SecondNumOfPoints {
+		l.Wins[firstTeam] += 1
+	} else if SecondNumOfPoints > FirstNumOfPoints {
+		l.Wins[secondTeam] += 1
+	}
+}
+
+func (l League) Ranking() []string {
+	keys := make([]string, 0, len(l.Wins))
+	for k := range l.Wins {
+		keys = append(keys, k)
+	}
+	slices.SortFunc(keys, func(a, b string) int {
+		return l.Wins[b] - l.Wins[a]
+	})
+
+	for _, k := range keys {
+		fmt.Printf("%s: %d\n", k, l.Wins[k])
+	}
+
+	return keys
+}
