1c3b7abd2acc95788ae00f2747c22c7ebaa7c74e
Michał <michal.lejwoda@gmail.com>
2026-05-31 11:54:57 +0200

    chapter_8

---
+++ exercises/chapter_8/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	fmt.Println(doubleValue(12))
+	fmt.Println(doubleValue(31))
+	var i PrintInt = 20
+	PrintIt(i)
+	var f PrintFloat = 10.23
+	PrintIt(f)
+	l := &List[int]{}
+	l.Add(5)
+	l.Add(10)
+	fmt.Println(l.Index(5))
+	fmt.Println(l.Index(10))
+	fmt.Println(l.Index(20))
+
+	l.Insert(100, 0)
+	fmt.Println(l.Index(5))
+	fmt.Println(l.Index(10))
+	fmt.Println(l.Index(20))
+	fmt.Println(l.Index(100))
+
+	l.Insert(200, 1)
+	fmt.Println(l.Index(5))
+	fmt.Println(l.Index(10))
+	fmt.Println(l.Index(200))
+	fmt.Println(l.Index(20))
+	fmt.Println(l.Index(100))
+}
+
+type Printable interface {
+	fmt.Stringer
+	~int | ~float64
+}
+
+func doubleValue[T int | float64](val T) T {
+	return val * 2
+}
+
+type PrintInt int
+
+func (pi PrintInt) String() string {
+	return strconv.Itoa(int(pi))
+}
+
+type PrintFloat float64
+
+func (pf PrintFloat) String() string {
+	return fmt.Sprintf("%f", pf)
+}
+
+func PrintIt[T Printable](t T) {
+	fmt.Println(t)
+}
+
+type Node[T comparable] struct {
+	Value T
+	Next  *Node[T]
+}
+
+type List[T comparable] struct {
+	Head *Node[T]
+	Tail *Node[T]
+}
+
+func (l *List[T]) Add(t T) {
+	n := &Node[T]{
+		Value: t,
+	}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+		return
+	}
+	l.Tail.Next = n
+	l.Tail = l.Tail.Next
+}
+
+func (l *List[T]) Insert(t T, pos int) {
+	n := &Node[T]{
+		Value: t,
+	}
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+	}
+	if pos <= 0 {
+		n.Next = l.Head
+		l.Head = n
+		return
+	}
+	curNode := l.Head
+	for i := 1; i < pos; i++ {
+		if curNode.Next == nil {
+			curNode.Next = n
+			l.Tail = curNode.Next
+			return
+		}
+	}
+	n.Next = curNode.Next
+	curNode.Next = n
+	if l.Tail == curNode {
+		l.Tail = n
+	}
+}
+
+func (l *List[T]) Index(t T) int {
+	i := 0
+	for curNode := l.Head; curNode != nil; curNode = curNode.Next {
+		if curNode.Value == t {
+			return i
+		}
+		i++
+	}
+	return -1
+}
