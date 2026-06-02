d6d20e3d762584859e31cd1a1619ce7a1861a059
Michał <michal.lejwoda@gmail.com>
2026-06-02 22:14:36 +0200

    chapter_9 second exercise

--- exercises/chapter_9/main.go
+++ exercises/chapter_9/main.go
@@ -7,12 +7,42 @@ import (
 
 var ErrDataNotFound = errors.New("data not found")
 
+type EmptyFieldError struct {
+	FieldName string
+}
+
+var ErrInvalidID = errors.New("invalid ID")
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Id        int
+}
+
+func (fe EmptyFieldError) Error() string {
+	return fe.FieldName
+}
+
 func main() {
 	fmt.Println("Hello World")
 	_, err := biggerThanSeven(8)
+	var fieldErr EmptyFieldError
+
 	if err != nil {
 		if errors.Is(err, ErrDataNotFound) {
 			fmt.Println("Data Not Found")
+		}
+	}
+	empl := Employee{
+		"sadasd",
+		"",
+		0,
+	}
+	err = validateEmployee(empl)
+	if err != nil {
+		fmt.Println(err)
+		if errors.As(err, &fieldErr) {
+			fmt.Println("Check if as working")
 		}
 	}
 }
@@ -23,4 +53,17 @@ func biggerThanSeven(num int) (int, error) {
 	} else {
 		return num, nil
 	}
+}
+
+func validateEmployee(e Employee) error {
+	if len(e.FirstName) == 0 {
+		return EmptyFieldError{FieldName: "FirstName"}
+	}
+	if len(e.LastName) == 0 {
+		return EmptyFieldError{FieldName: "LastName"}
+	}
+	if e.Id == 0 {
+		return ErrInvalidID
+	}
+	return nil
 }
