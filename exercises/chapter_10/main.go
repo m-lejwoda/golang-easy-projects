bb527e4c0c1c9f5ba164189434a18138e4c35ab8
Michał <michal.lejwoda@gmail.com>
2026-06-04 21:26:46 +0200

    v1.0.1

---
+++ exercises/chapter_10/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"golang_easy_projects/exercises/chapter_10/utils"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	res := utils.Add(23, 43)
+	fmt.Println("res", res)
+}
---
+++ exercises/chapter_10/utils/utils.go
@@ -0,0 +1,8 @@
+// Simple uttils package
+package utils
+
+// Add take two ints and return sum of two values
+// More information you will find on page https://www.mathisfun.com/numbers/addition.html
+func Add(a int, b int) int {
+	return a + b
+}
